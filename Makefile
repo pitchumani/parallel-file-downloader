2dd3829dceabad3e75babb8de95b9bb76deb7980
Pitchumani Sivanupandi <s.pitchumani@gmail.com>
2025-06-11 23:23:59 -0400

    Implemented sequential file downloads using libcurl

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 # Makefile for parallel-file-downloader
 CXX = clang++
 CXXFLAGS = -std=c++17 -Wall -Wextra -O2
+LDFLAGS = -lcurl
+GTEST_LDFLAGS = -lgtest -lgtest_main -lcurl
+
 SRC_DIR = $(realpath ./src)
 BUILD_DIR = $(SRC_DIR)/../build
-SRC = $(SRC_DIR)/main.cpp
+SRC = $(wildcard $(SRC_DIR)/*.cpp)
 TARGET = $(BUILD_DIR)/pfd
 GTEST_DIR = $(realpath ./gtest)
 GTEST_SRC = $(wildcard $(GTEST_DIR)/*.cpp)
@@ -14,7 +17,7 @@ $(BUILD_DIR):
 	mkdir -p $(BUILD_DIR)
 
 $(TARGET): $(SRC) | $(BUILD_DIR)
-	$(CXX) $(CXXFLAGS) -o $@ $<
+	$(CXX) $(CXXFLAGS) -o $@ $(SRC) $(LDFLAGS)
 
 .PHONY: clean gtest
 
@@ -23,5 +26,5 @@ clean:
 
 gtest: $(GTEST_SRC) | $(BUILD_DIR)
 	@echo "Building and running Google Test (gtest) target..."
-	$(CXX) $(CXXFLAGS) -pthread -lgtest -lgtest_main -o $(BUILD_DIR)/gtest $(GTEST_SRC)
-	$(BUILD_DIR)/gtest
+	$(CXX) $(CXXFLAGS) -o $(BUILD_DIR)/gtest $(GTEST_SRC) $(GTEST_LDFLAGS)
+	cd build && ./gtest
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,20 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <curl/curl.h>
+#include <fstream>
+#include <stdexcept>
 
+#include "curl.h"
 
-int main (int argc, char *argv[]) {
+std::string basename(const std::string &path) {
+    size_t pos = path.find_last_of('/');
+    if (pos == std::string::npos) {
+        return path; // No directory part, return the whole path
+    }
+    return path.substr(pos + 1); // Return the part after the last '/'
+}
+
+int main (int, char **) {
     std::cout << "Parallel Downloading Example" << std::endl;
 
     // list of URLs to download
@@ -16,10 +26,20 @@ int main (int argc, char *argv[]) {
     urls.push("https://ftp.gnu.org/gnu/bash/bash-2.04.tar.gz");
     urls.push("https://ftp.gnu.org/gnu/bash/bash-2.05.tar.gz");
 
+    // Initialize CURL globally
+    CurlDownload::globalInit();
+    // Create a downloader instance
+    CurlDownload downloader;
+    // Download each URL in the queue
     while (!urls.empty()) {
         std::string url = urls.front();
         urls.pop();
-        std::cout << "Downloading: " << url << std::endl;
+        std::string outputFile = basename(url);
+
+        std::cout << "Downloading: " << url << " to '" << outputFile << "'" << std::endl;
+        if (!downloader.download(url, outputFile)) {
+            std::cerr << "Failed to download: " << url << std::endl;
+        }
     }
 
     return 0;
